51f75c0fade50f5f6d3d812c3a9ba2d689ff2ca2
onestardev <faonestar514@gmail.com>
2017-10-16 08:17:37 +0200

    eventhandler

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-var fs = require("fs");
+// Import events module
+var events = require('events');
 
-fs.readFile('input.rtf', function (err, data) {
-   if (err) return console.error(err);
-   console.log(data.toString());
+
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+// Create an event handler as follows
+var connectHandler = function connected() {
+   console.log('connection succesful.');
+  
+   // Fire the data_received event 
+   eventEmitter.emit('data_received');
+}
+
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectHandler);
+ 
+// Bind the data_received event with the anonymous function
+eventEmitter.on('data_received', function(){
+   console.log('data received succesfully.');
 });
 
-console.log("Program Ended");
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+console.log("Program Ended.");
