e0cd19e3f7c2ff10ae5338d34ab49b2c0343f227
onestardev <faonestar514@gmail.com>
2017-10-17 03:44:28 +0200

    stream2

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
 var fs = require("fs");
-var data = 'Simply Easy Learning';
+
+// Create a readable stream
+var readerStream = fs.createReadStream('input.rtf');
 
 // Create a writable stream
 var writerStream = fs.createWriteStream('output.txt');
 
-// Write the data to stream with encoding to be utf8
-writerStream.write(data,'UTF8');
-
-// Mark the end of file
-writerStream.end();
-
-// Handle stream events --> finish, and error
-writerStream.on('finish', function() {
-    console.log("Write completed.");
-});
-
-writerStream.on('error', function(err){
-   console.log(err.stack);
-});
+// Pipe the read and write operations
+// read input.txt and write data to output.txt
+readerStream.pipe(writerStream);
 
 console.log("Program Ended");
