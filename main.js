baef476985643ab8c105b9196a031b07be833b88
onestardev <faonestar514@gmail.com>
2017-10-18 22:36:45 +0200

    file system

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 var fs = require("fs");
-var zlib = require('zlib');
 
-// Compress the file input.txt to input.txt.gz
-fs.createReadStream('input.txt')
-   .pipe(zlib.createGzip())
-   .pipe(fs.createWriteStream('input.txt.gz'));
-  
-console.log("File Compressed.");
+// Asynchronous read
+fs.readFile('input.txt', function (err, data) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log("Asynchronous read: " + data.toString());
+});
+
+// Synchronous read
+var data = fs.readFileSync('input.txt');
+console.log("Synchronous read: " + data.toString());
+
+console.log("Program Ended");
