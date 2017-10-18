f1b5f5260571e5f3ad97110d7a663b5092d9a1a1
onestardev <faonestar514@gmail.com>
2017-10-18 22:49:44 +0200

    reading with parameter

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 var fs = require("fs");
+var buf = new Buffer(1024);
 
-console.log("Going to write into existing file");
-fs.writeFile('input.rtf', 'Simply Easy Learning!',  function(err) {
+console.log("Going to open an existing file");
+fs.open('input.txt', 'r+', function(err, fd) {
    if (err) {
       return console.error(err);
    }
-   
-   console.log("Data written successfully!");
-   console.log("Let's read newly written data");
-   fs.readFile('input.rtf', function (err, data) {
-      if (err) {
-         return console.error(err);
+   console.log("File opened successfully!");
+   console.log("Going to read the file");
+   fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+      if (err){
+         console.log(err);
       }
-      console.log("Asynchronous read: " + data.toString());
+      console.log(bytes + " bytes read");
+      
+      // Print only read bytes to avoid junk.
+      if(bytes > 0){
+         console.log(buf.slice(0, bytes).toString());
+      }
    });
 });
