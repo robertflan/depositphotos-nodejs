463670a81d272c1e0c94cca97663fc573176648f
onestardev <faonestar514@gmail.com>
2017-10-18 22:44:49 +0200

    file write

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 var fs = require("fs");
 
-// Asynchronous read
-fs.readFile('input.txt', function (err, data) {
+console.log("Going to write into existing file");
+fs.writeFile('input.rtf', 'Simply Easy Learning!',  function(err) {
    if (err) {
       return console.error(err);
    }
-   console.log("Asynchronous read: " + data.toString());
+   
+   console.log("Data written successfully!");
+   console.log("Let's read newly written data");
+   fs.readFile('input.rtf', function (err, data) {
+      if (err) {
+         return console.error(err);
+      }
+      console.log("Asynchronous read: " + data.toString());
+   });
 });
-
-// Synchronous read
-var data = fs.readFileSync('input.txt');
-console.log("Synchronous read: " + data.toString());
-
-console.log("Program Ended");
