0481f8f6a1d37bcbd2535bc0e362216d6ecbac0d
onestardev <faonestar514@gmail.com>
2017-10-30 23:02:21 +0100

    bye

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function printHello(){
-    console.log( "Hello, World!");
+    console.log( "Hello, World! good bye");
  }
  // Now call above function after 2 seconds
  setInterval(printHello, 2000);
